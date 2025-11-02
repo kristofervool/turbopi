d647baabb5049d6164d81e4b23d56b077f1d9ace
kristofervool <kristofer.vool@pipedrive.com>
2025-11-02 11:12:13 +0200

    prettier

--- src/client/src/pages/Library.tsx
+++ src/client/src/pages/Library.tsx
@@ -127,7 +127,7 @@ export default function Library() {
 
       {!loading && movies.length > 0 && (
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
-          {movies.map((movie) => (
+          {movies.map(movie => (
             <MovieCard
               key={movie.id}
               movie={movie}
